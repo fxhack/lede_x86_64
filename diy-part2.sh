69555456ac61cb5272e5d3acbb69c23a4a79bc5e
fxhack <fxhack@qq.com>
2020-12-30 18:12:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #增加软件包
-git clone https://github.com/liuran001/openwrt-packages package
+#git clone https://github.com/liuran001/openwrt-packages package
