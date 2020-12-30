85aa4dcb20b822c213eccf7c8b8e1ec599665a10
fxhack <fxhack@qq.com>
2020-12-30 18:24:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#添加软件包
+git clone https://github.com/liuran001/openwrt-packages.git package/openwrt-packages
